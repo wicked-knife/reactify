77101274169979125f86a37711ef568608c83a1f
wickde-knife <1353640486@qq.com>
2020-07-09 23:03:02 +0800

    add button size styles

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
     <div className='App'>
       {buttons.map(
         buttonType => (
-          <Button type={buttonType} key={buttonType} onClick={clickHandler} round block>
+          <Button type={buttonType} key={buttonType} onClick={clickHandler} round size="small" >
             {buttonType}
           </Button>
         )
@@ -23,7 +23,7 @@ const App: React.FC = () => {
       <div style={{marginTop: '100px'}}>
       {buttons.map(
         buttonType => (
-          <Button type={buttonType} key={buttonType} onClick={clickHandler} plain block>
+          <Button type={buttonType} key={buttonType} onClick={clickHandler} plain size="small" >
             {buttonType}
           </Button>
         )
@@ -33,7 +33,7 @@ const App: React.FC = () => {
       <div style={{marginTop: '100px'}}>
       {buttons.map(
         buttonType => (
-          <Button type={buttonType} key={buttonType} onClick={clickHandler} block>
+          <Button type={buttonType} key={buttonType} onClick={clickHandler} >
             {buttonType}
           </Button>
         )
