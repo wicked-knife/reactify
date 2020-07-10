da22e5587a4122c70bb0ac01dc1fcdf64155aacf
wickde-knife <1353640486@qq.com>
2020-07-10 21:29:50 +0800

    use mixins to minimize code

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
       <div style={{marginTop: '100px'}}>
       {buttons.map(
         buttonType => (
-          <Button type={buttonType} key={buttonType} onClick={clickHandler} round size="medium" >
+          <Button type={buttonType} key={buttonType} onClick={clickHandler} round size="medium" plain >
             {buttonType}
           </Button>
         )
