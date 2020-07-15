f2587db5c5ea72bd95f93839ce79c76474142746
geraltfeng <geraltfeng@tencent.com>
2020-07-15 19:56:33 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,6 @@ import './styles/index.scss'
 const App: React.FC = () => {
   const messageType: Array<MessageType> = ['info', 'warning', 'error', 'success']
 
-  const handleClick = () => {
-    Message.info('xxx')
-  }
   return <div>
     {messageType.map(type => 
       <Message key={type} type={type} className="mb-2">hello world</Message>
