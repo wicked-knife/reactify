7f19f8a00442c9f6d2e44b45e8116bfee5170f37
geraltfeng <geraltfeng@tencent.com>
2020-07-15 18:39:22 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React, { } from 'react'
 import Message, {MessageType} from './components/message/message'
+import Button from './components/button/button'
 import './styles/index.scss'
 
 const App: React.FC = () => {
   const messageType: Array<MessageType> = ['info', 'warning', 'error', 'success']
+
+  const handleClick = () => {
+    Message.info('xxx')
+  }
   return <div>
     {messageType.map(type => 
-      <Message key={type} type={type} className="mb-2"/>
+      <Message key={type} type={type} className="mb-2">hello world</Message>
     )}
+
+    <Button onClick={handleClick}>click me</Button>
   </div>
 }
 
--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -1,32 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import ReactDOM from 'react-dom'
 import useClassNames from 'classnames'
 import Icon from '../icon/icon'
+import { CSSTransition } from 'react-transition-group'
 import './message.scss'
 
 export type MessageType = 'success' | 'warning' | 'error' | 'info'
+
+export type MessageOptions = {
+  message?: string,
+  duration?: number,
+  onClose?: () => void,
+}
+
+const defaults: MessageOptions = {
+  message: '',
+  duration: 3000,
+  onClose: () => {}
+}
+
+interface MessageFunc {
+  (opt: MessageOptions | string) : void
+}
 
 interface MessageProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
   type?: MessageType
 }
 
-const Message: React.FC<MessageProps> = ({
+interface MessageInterface extends React.FC<MessageProps>{
+  info: MessageFunc,
+  // error: MessageFunc,
+  // success: MessageFunc,
+  // warning: MessageFunc,
+  // warn: MessageFunc,
+  // alert: MessageFunc
+}
+
+const Message: MessageInterface = ({
   type,
   className,
   children
 }) => {
+  const [visibility, setVisibility] = useState(false)
   const computedClassNames = useClassNames(
     'rf-message',
     `rf-message-${type}`,
     className
   )
+  const nodeRef = useRef(null)
 
-  return <div className={computedClassNames}>
-    <Icon className="icon-info mr-2" size={20} />  {children}
-  </div>
+  useEffect(() => {
+    setTimeout(() => {
+      setVisibility(true)
+    }, 1000);
+  }, [])
+
+  return (
+    <CSSTransition in={visibility} timeout={300} classNames='message' unmountOnExit nodeRef={nodeRef}>
+      <div className={computedClassNames} ref={nodeRef}>
+        <Icon className="icon-info mr-2" size={20} />  {children}
+      </div>
+    </CSSTransition>
+  )
+}
+
+Message.info = (opt) => {
+  let options: MessageOptions = {}
+  if(typeof opt === 'string') {
+    options = {...defaults, message: opt}
+  } else {
+    options = {...defaults, ...opt}
+  }
+
+  const element = ReactDOM.render(<Message type='info'>{options.message}</Message>, document.createDocumentFragment())
+  console.log(element);
 }
 
 Message.defaultProps = {
   type: 'info'
 }
+
 
 export default Message
