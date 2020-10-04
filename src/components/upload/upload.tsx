d1ef8c7d59377e2c5afdf6c73e52a56148571df7
wicked-knife <1353640486@qq.com>
2020-10-04 11:52:51 +0800

    trigger click

--- src/components/upload/upload.tsx
+++ src/components/upload/upload.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Button from '../button'
 import {Upload as IconUpload} from '../icon'
 
 const Upload = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const ck = () => {
+    inputRef.current!.click()
+  }
   return <div>
-    <input type="file" style={{display: 'none'}}/>
-    <Button><IconUpload /> upload</Button>
+    <input type="file" style={{display: 'none'}} ref={inputRef}/>
+    <Button onClick={ck}><IconUpload /> upload</Button>
   </div>
 }
 
