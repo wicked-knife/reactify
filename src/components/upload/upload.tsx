ca7dcd42ba37d44978161248c673e2e5ffe72970
geraltfeng <geraltfeng@tencent.com>
2020-10-10 16:35:57 +0800

    update

--- src/components/upload/upload.tsx
+++ src/components/upload/upload.tsx
@@ -1,16 +1,26 @@
-import React, { useRef } from 'react'
+import React, { ChangeEvent, useRef } from 'react'
 import Button from '../button'
 import {Upload as IconUpload} from '../icon'
+import './index.scss'
 
 const Upload = () => {
   const inputRef = useRef<HTMLInputElement>(null)
 
-  const ck = () => {
+  const handleUploadButtonClick = () => {
     inputRef.current!.click()
   }
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    event.persist()
+    console.log(event.target.files)
+  }
   return <div>
-    <input type="file" style={{display: 'none'}} ref={inputRef}/>
-    <Button onClick={ck}><IconUpload /> upload</Button>
+    <input type="file" style={{display: 'none'}} ref={inputRef} onChange={handleFileChange}/>
+    <Button onClick={handleUploadButtonClick}>
+      <div className="rf-upload-btn">
+        <IconUpload /> upload
+      </div>
+    </Button>
   </div>
 }
 
