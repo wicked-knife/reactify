48c392987ec96063c28b45f6158012cabe131b4a
wickde-knife <1353640486@qq.com>
2020-10-11 22:12:02 +0800

    update

--- src/components/upload/upload.tsx
+++ src/components/upload/upload.tsx
@@ -1,9 +1,13 @@
-import React, { ChangeEvent, useRef } from 'react'
+import React, { ChangeEvent, useRef, FC} from 'react'
 import Button from '../button'
 import {Upload as IconUpload} from '../icon'
 import './index.scss'
 
-const Upload = () => {
+export interface UploadPropsInterface {
+  accept?: string
+}
+
+const Upload: FC<UploadPropsInterface> = ({accept}) => {
   const inputRef = useRef<HTMLInputElement>(null)
 
   const handleUploadButtonClick = () => {
@@ -15,10 +19,10 @@ const Upload = () => {
     console.log(event.target.files)
   }
   return <div>
-    <input type="file" style={{display: 'none'}} ref={inputRef} onChange={handleFileChange}/>
+    <input type="file" style={{display: 'none'}} ref={inputRef} onChange={handleFileChange} accept={accept}/>
     <Button onClick={handleUploadButtonClick}>
       <div className="rf-upload-btn">
-        <IconUpload /> upload
+        <IconUpload className="upload-icon"/> upload
       </div>
     </Button>
   </div>
