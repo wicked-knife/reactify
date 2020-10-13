80e3f7b805be310ae14fa344bf59f8c3c62525d5
geraltfeng <geraltfeng@tencent.com>
2020-10-13 12:56:09 +0800

    update

--- src/components/upload/uploadRequest.ts
+++ src/components/upload/uploadRequest.ts
@@ -1,9 +1,37 @@
 interface UploadOptions {
-  
+  action: string
+  name?: string
+  headers?: object
+  data?: object
+  withCredentials?: boolean
+  onStart?: (files: File | File[] ) => void
+  // TODO: onProgress onSuccess onError
 }
 
-const upload = () => {
-  
+const noop = () => {}
+
+const uploadOptionsDefaults: Omit<UploadOptions, 'action'> = {
+  name: 'file',
+  withCredentials: false,
+  onStart: noop
+}
+
+const upload = (uploadOptions: UploadOptions) => {
+  const opt = {...uploadOptions, ...uploadOptionsDefaults}
+  const xhr = new XMLHttpRequest()
+  const formData = new FormData()
+
+  if(opt.headers) {
+    Object.entries(opt.headers).forEach(keyValue => {
+      const [key, value] = keyValue
+      xhr.setRequestHeader(key, value)
+    })
+  }
+
+  if(opt.withCredentials) {
+    xhr.withCredentials = true
+  }
+
 }
 
 export default upload
