355b64e035507d787357de3ec53fd0cb7d10d057
wickde-knife <1353640486@qq.com>
2020-10-17 21:38:28 +0800

    update

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,38 +1,34 @@
-import React from 'react';
+import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta, } from '@storybook/react/types-6-0'
 
-import { Button, ButtonProps } from './Button';
+import Button, { ButtonProps } from '../components/button/index'
 
 export default {
   title: 'Example/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as Meta;
+} as Meta
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => <Button {...args} >button</Button>
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 Primary.args = {
-  primary: true,
-  label: 'Button',
-};
+  type: 'primary'
+}
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+// export const Secondary = Template.bind({})
+// Secondary.args = {
+//   label: 'Button',
+// }
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+// export const Large = Template.bind({})
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// }
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+// export const Small = Template.bind({})
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// }
