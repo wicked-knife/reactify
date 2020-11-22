b9086eb745490e4ea81c3a0bb15db576e9092db1
wickde-knife <1353640486@qq.com>
2020-11-22 12:14:25 +0800

    update

---
+++ src/components/icon/components/BaseIcon.tsx
@@ -0,0 +1,38 @@
+import React, {
+  ForwardRefRenderFunction,
+  forwardRef,
+  SVGProps,
+} from 'react';
+import useClassNames from 'classnames';
+
+export interface BaseIconProps extends SVGProps<SVGElement> {
+  color?: string;
+  size?: number | string;
+  className?: string;
+  Icon: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+}
+
+const BaseIcon: ForwardRefRenderFunction<any, BaseIconProps> = (
+    {color, size, className, Icon, ...props},
+    ref,
+) => {
+  const computedClassNames = useClassNames('iconfont', 'rf-icon', className);
+  const attrs = {
+    width: size,
+    height: size,
+    color,
+  };
+  return (
+    <Icon className={computedClassNames} {...props} {...attrs} ref={ref} />
+  );
+};
+
+const Icon = forwardRef(BaseIcon);
+
+Icon.defaultProps = {
+  size: 16,
+};
+
+export default Icon;
--- src/components/icon/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Demo from './svg/demo.svg';
-
-const Icon = () => <div>
-  <Demo className="foo bar"></Demo>
-</div>;
-
-export default Icon;
