ae9221436c4fd8dd1127ce3f32b1f3808c17e0e3
wickde-knife <1353640486@qq.com>
2021-01-04 22:04:57 +0800

    fix: try to fix jest with svgr

--- jest.config.ts
+++ jest.config.ts
@@ -81,6 +81,7 @@ module.exports = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.svg': '<rootDir>/__mocks__/svgrMock.js',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
