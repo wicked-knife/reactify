d6959e46aec1f843082ac6828c9e9acd66d3002b
wickde-knife <1353640486@qq.com>
2021-01-16 19:35:53 +0800

    update fix

--- __mocks__/svgrMock.js
+++ __mocks__/svgrMock.js
@@ -1,4 +1,4 @@
 import * as React from 'react';
-export default 'SvgrURL';
+export default 'svgr-mock';
 const SvgrMock = React.forwardRef((props, ref) => <span ref={ref} {...props} />);
 export const ReactComponent = SvgrMock;
